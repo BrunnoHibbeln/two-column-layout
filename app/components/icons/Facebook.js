cc2c58232f9d0792c82fe63d43ef91d7a342d947
Brunno Hibbeln <brunnohibbeln10@gmail.com>
2023-09-19 19:02:01 +0000

    modifying `icons`

---
+++ app/components/icons/Facebook.js
@@ -0,0 +1,16 @@
+export default function Facebook() {
+  return (
+    <a href="#">
+      <svg
+        className="h-8 w-8 rounded-full border border-white p-1 hover:border-blue hover:fill-blue"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="#fff"
+        width="800px"
+        height="800px"
+        viewBox="0 0 32 32"
+      >
+        <path d="M21.95 5.005l-3.306-.004c-3.206 0-5.277 2.124-5.277 5.415v2.495H10.05v4.515h3.317l-.004 9.575h4.641l.004-9.575h3.806l-.003-4.514h-3.803v-2.117c0-1.018.241-1.533 1.566-1.533l2.366-.001.01-4.256z" />
+      </svg>
+    </a>
+  )
+}
